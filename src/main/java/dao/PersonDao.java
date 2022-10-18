ecbd60cb6b578e7e0ffd217ed0fd4f26d458f2bd
Cole Russon <106703710+colerusson@users.noreply.github.com>
2022-10-18 10:44:33 -0600

    New commit

--- src/main/java/dao/PersonDao.java
+++ src/main/java/dao/PersonDao.java
@@ -1,8 +1,12 @@
 package dao;
 
+import model.Event;
 import model.Person;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -29,6 +33,26 @@ public class PersonDao {
      * @throws DataAccessException error in accessing the table
      */
     public void insert(Person person) throws DataAccessException {
+        String sql = "INSERT INTO Person (personID, associatedUsername, firstName, lastName, gender, " +
+                "fatherID, motherID, spouseID) VALUES(?,?,?,?,?,?,?,?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            //Using the statements built-in set(type) functions we can pick the question mark we want
+            //to fill in and give it a proper value. The first argument corresponds to the first
+            //question mark found in our sql String
+            stmt.setString(1, person.getPersonID());
+            stmt.setString(2, person.getAssociatedUsername());
+            stmt.setString(3, person.getFirstName());
+            stmt.setString(4, person.getLastName());
+            stmt.setString(5, person.getGender());
+            stmt.setString(6, person.getFatherID());
+            stmt.setString(7, person.getMotherID());
+            stmt.setString(8, person.getSpouseID());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DataAccessException("Error encountered while inserting an event into the database");
+        }
     }
 
     /**
@@ -37,8 +61,25 @@ public class PersonDao {
      * @return the person method object for the respective person
      * @throws DataAccessException error in accessing the table
      */
-    public Person getPersonByID(String personID) throws DataAccessException {
-        return null;
+    public Person find(String personID) throws DataAccessException {
+        Person person;
+        ResultSet rs;
+        String sql = "SELECT * FROM Person WHERE personID = ?;";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, personID);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                person = new Person(rs.getString("personID"), rs.getString("associatedUsername"),
+                        rs.getString("firstName"), rs.getString("lastName"), rs.getString("gender"),
+                        rs.getString("fatherID"), rs.getString("motherID"), rs.getString("spouseID"));
+                return person;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DataAccessException("Error encountered while finding an event in the database");
+        }
     }
 
     /**
@@ -82,7 +123,14 @@ public class PersonDao {
      * method to delete the table
      * @throws DataAccessException error in accessing the table
      */
-    public void deleteTable() throws DataAccessException {
+    public void clear() throws DataAccessException {
+        String sql = "DELETE FROM Person";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DataAccessException("Error encountered while clearing the event table");
+        }
     }
 
 }
